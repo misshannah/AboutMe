379ed307b3bde2ac6e3f3bded017ead1350a854d
hannaholukoye <hannaholukoye@gmail.com>
2019-07-02 18:05:18 +0300

    Upload source code

---
+++ app/src/main/java/com/olukoye/hannah/aboutme/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.olukoye.hannah.aboutme
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.olukoye.hannah.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname (view: View) {
+
+        binding.apply {
+
+
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+
+        }
+
+    }
+
+}
